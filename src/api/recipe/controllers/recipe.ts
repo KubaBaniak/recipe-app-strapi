448c4cbb2d0721434f0203b39951b99991058bec
Jakub Urbaniak <kuba121201@gmail.com>
2023-11-14 12:02:07 +0100

    feat: add user as author to recipe

--- src/api/recipe/controllers/recipe.ts
+++ src/api/recipe/controllers/recipe.ts
@@ -2,6 +2,10 @@
  * recipe controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::recipe.recipe');
+export default factories.createCoreController('api::recipe.recipe', {
+  async create(ctx) {
+    return await strapi.services['api::recipe.recipe'].create(ctx);
+  },
+});
